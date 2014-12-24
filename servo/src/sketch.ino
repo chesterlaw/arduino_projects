6f036e4a8d54898d8cdd250331b1c6db1ed55677
Chester Law <chesterlaw@me.com>
2014-12-24 15:20:16 -0800

    Make buzzer buzz on button release

--- servo/src/sketch.ino
+++ servo/src/sketch.ino
@@ -1,10 +1,13 @@
 // A servo controlled by a potentiometer
+// and a doorbell
 #include <Servo.h>  // servo library
 
 Servo servo1;  // servo control object
 int sensorPin = 0; // The potentiometer is connected to analog pin 0
 const int buzzerPin = 10; // designate buzzer pin
-const int buttonPin = 2;  // pushbutton pin
+const int buttonPin = 8;  // pushbutton pin
+boolean currentState = LOW;
+boolean lastState = LOW;
 
 void setup() {
   servo1.attach(9); //attach servo1 object to diginal pin 9
@@ -17,13 +20,12 @@ void loop() {
   int sensorValue = analogRead(sensorPin); // get potentiometer value
 
   int degreesToRotate = sensorValue/1023.0 * 180; // potentiometer goes from 0 to 1023
-  // Serial.println(degreesToRotate);
 
   int delayAmount = 0;
   servo1.write(degreesToRotate); // turn (between 0 and 180)
   delay(delayAmount); // refresh time
 
-  // Buzzer
+  // Buzz on reaching min and max
   int toneLength = 5;
   // Low C on 0 degrees, High C on reaching 180
   if (degreesToRotate == 0) {
@@ -32,11 +34,15 @@ void loop() {
     tone(buzzerPin, 523, toneLength);
   }
 
-  // Button
-  int buttonState = digitalRead(buttonPin);
-  Serial.println(buttonState);
-
-  if (buttonState == LOW) {
-    tone(buzzerPin, 349, toneLength);    
+  // Pin Pom Doorbell
+  int pinPomDuration = 300;
+  int currentState = digitalRead(buttonPin);
+    
+  if (currentState == LOW) { // pressed
+    tone(buzzerPin, 330, toneLength);
+  } else if (currentState == HIGH && lastState == LOW) { // let go
+    tone(buzzerPin, 262, pinPomDuration);
   }
+  lastState = currentState;
 }
+
